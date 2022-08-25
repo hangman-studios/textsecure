7e290f2c762f2f6fc5d1960ffb79668f0223bf97
Jan-Niclas Gladbach <jniclasg@googlemail.com>
2022-08-25 12:15:12 +0200

    add header to logs in put requests

--- transport/transport.go
+++ transport/transport.go
@@ -191,7 +191,7 @@ func (ht *httpTransporter) Del(url string) (*response, error) {
 	return r, err
 }
 
-func PrintBody(resp *http.Response, err error, functionName string) {
+func PrintBodyAndHeader(resp *http.Response, err error, functionName string) {
 	bodyBytes, readerr := ioutil.ReadAll(resp.Body)
 	if readerr != nil {
 		log.Debugf("[textsecure] %s while reading body %s\n", functionName, readerr)
@@ -201,7 +201,15 @@ func PrintBody(resp *http.Response, err error, functionName string) {
 		log.Debugf("[textsecure] %s while closing body %s\n", functionName, closeErr)
 	}
 	resp.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
-	log.Debugf("[textsecure] %s response: %+v\nError: %+v\nBody: %+v\nBody read: %s\n", functionName, resp, err, resp.Body, bodyBytes)
+	log.Debugf("[textsecure] %s response: %+v", functionName, resp)
+	log.Debugf("Error: %+v", err)
+	log.Debugf("Headers:")
+	for key, value := range resp.Header {
+		log.Debugf("  \"%s\" = \"%s\"", key, value)
+	}
+	log.Debugf("Body: %+v", resp.Body)
+	log.Debugf("Body read: %s", bodyBytes)
+
 }
 
 func (ht *httpTransporter) Put(url string, body []byte, ct string) (*response, error) {
@@ -216,7 +224,7 @@ func (ht *httpTransporter) Put(url string, body []byte, ct string) (*response, e
 	req.Header.Add("Content-Type", ct)
 	req.SetBasicAuth(ht.user, ht.pass)
 	resp, err := ht.client.Do(req)
-	PrintBody(resp, err, "PUT")
+	PrintBodyAndHeader(resp, err, "PUT")
 	if err != nil {
 		return nil, err
 	}
@@ -243,7 +251,7 @@ func (ht *httpTransporter) PutWithAuth(url string, body []byte, ct string, auth 
 	req.Header.Add("Content-Type", ct)
 	req.Header.Set("Authorization", auth)
 	resp, err := ht.client.Do(req)
-	PrintBody(resp, err, "PutWithAuth")
+	PrintBodyAndHeader(resp, err, "PutWithAuth")
 	if err != nil {
 		return nil, err
 	}
@@ -273,7 +281,7 @@ func (ht *httpTransporter) PutWithUnidentifiedSender(url string, body []byte, ct
 	unidentifiedAccessKeyBase64 := helpers.Base64EncWithoutPadding(unidentifiedAccessKey)
 	req.Header.Set("Unidentified-Access-Key", unidentifiedAccessKeyBase64)
 	resp, err := ht.client.Do(req)
-	PrintBody(resp, err, "PutWithUnidentifiedSender")
+	PrintBodyAndHeader(resp, err, "PutWithUnidentifiedSender")
 	if err != nil {
 		return nil, err
 	}
@@ -305,7 +313,7 @@ func (ht *httpTransporter) PutWithAuthCookies(url string, body []byte, ct string
 	req.Header.Set("Authorization", auth)
 
 	resp, err := ht.client.Do(req)
-	PrintBody(resp, err, "PutWithAuthCookies")
+	PrintBodyAndHeader(resp, err, "PutWithAuthCookies")
 	if err != nil {
 		return nil, err
 	}
